cf99821e177124305e16c5ab745c43f2553acdc2
João Paulo <nascimentojava@gmail.com>
2022-09-29 16:47:35 -0300

    Adicionei a verificação if-else

--- media.js
+++ media.js
@@ -12,8 +12,17 @@ function media() {
     let n2 = Number(nota2field.value);
     let n3 = Number(nota3field.value);
 
+    let media = (n1+n2+n3)/3;
+
     let secao = document.getElementById("output");
-    secao.innerHTML = `<h1>${(n1+n2+n3)/3}</h1>`;
+    secao.innerHTML = `<h1>${media}</h1><br>`;
+
+    if (media >= 7)
+        secao.innerHTML += `<h2>Aprovado<h2>`;
+    else if (media >= 4)
+    secao.innerHTML += `<h2>Recuperacao<h2>`;
+    else
+        secao.innerHTML += `<h2>Reprovado<h2>`;
 }
 
 
